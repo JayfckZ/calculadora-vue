e903a0ad4df03e7f1aed5a52b8a67f7af3aa5129
Jay <joaomarcelo1015ja@gmail.com>
2024-04-02 19:01:39 -0300

    Finalizando o projeto

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { reactive } from 'vue';
+import Formulario from './components/Formualrio.vue'
+
+const estado = reactive({
+  acao: 'adicao',
+  valor1: 0,
+  valor2: 0
+})
+
+const operacao = () => {
+  let { acao, valor1, valor2 } = estado
+
+  switch (acao) {
+    case 'adicao':
+      return valor1 + valor2
+    case 'subtracao':
+      return valor1 - valor2
+    case 'multiplicacao':
+      return valor1 * valor2
+    case 'divisao':
+      if (valor2 == 0) {
+        return 'Não é possível'
+      } else {
+        return valor1 / valor2
+      }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="text-center mt-5">Calculadora Aritmética</h1>
+      <Formulario :primeiro-valor="e => estado.valor1 = e.target.value" :segundo-valor="e => estado.valor2 = e.target.value" :muda-acao="e => estado.acao= e.target.value" :operacao="operacao()"/>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/Formualrio.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps(['primeiroValor', 'segundoValor', 'mudaAcao', 'operacao'])
+</script>
+
+<template>
+    <section>
+        <div class="row">
+            <div class="col-sm-3 col-md-5"></div>
+
+            <form class="col-sm-6 col-md-2 bg-light p-4 rounded mt-2">
+                <input type="number" class="form-control text-center" placeholder="Primeiro número" value="0" @keyup="props.primeiroValor">
+                <select class="form-control mt-2 text-center" @change="props.mudaAcao">
+                    <option value="adicao">Somar</option>
+                    <option value="subtracao">Subtrair</option>
+                    <option value="multiplicacao">Multiplicar</option>
+                    <option value="divisao">Dividir</option>
+                </select>
+                <input type="number" class="form-control text-center mt-2 mb-3" placeholder="Segundo número" value="0" @keyup="props.segundoValor">
+
+                <h5>Resultado:</h5>
+                <h4 class="text-center mt-4">{{ props.operacao }}</h4>
+            </form>
+
+            <div class="col-sm-3 col-md-5"></div>
+        </div>
+    </section>
+</template>
